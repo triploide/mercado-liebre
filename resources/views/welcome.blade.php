ef4b5fe0ad3b560b559f38e71596e8c3998ed3df
Maxi Yañez <digitalhouse@DH-A3-02.digitalhouse.local>
2016-11-15 10:59:18 -0300

    cambios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Edit
                 </div>
 
                 <div class="links">
